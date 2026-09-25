443e1891d26e1059e6d69b50e261ad40965e3fcc
quantriphung-web <quantriphung@gmail.com>
2026-09-25 23:54:11 +0700

    Delete frontend directory

--- frontend/lib/routes.dart
+++
@@ -1,8 +0,0 @@
-import 'package:food_delivery_app/pages/home_page.dart';
-import 'package:food_delivery_app/pages/splash_page.dart';
-import 'package:get/get.dart';
-
-class Routes {
-  static String splashPages = '/splash';
-  static String homePages = '/';
-}
--- frontend/lib/values/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primaryColor = Color(0xffff7622);
-  static const Color bgButtonColor = Color(0xffecf0f4);
-}
